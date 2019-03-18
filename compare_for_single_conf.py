6119d097efce7f7dfc721ddc66ed3534ab4effe4
William Kavanagh <bkav93@gmail.com>
2019-03-18 21:05:50 +0000

    STATS eugh

--- compare_for_single_conf.py
+++ compare_for_single_conf.py
@@ -42,20 +42,33 @@ def plot_result(file):
     KA_var = KA_total_var / l_last_three_quarters
     KW_var = KW_total_var / l_last_three_quarters
     AW_var = AW_total_var / l_last_three_quarters
+    variances = [KA_var, KW_var, AW_var]
 
-    std = np.std([KA_var, KW_var, AW_var], dtype=np.float64)
-    mu = np.mean([KA_var, KW_var, AW_var])
-    return y, std, mu
+    std = np.std(variances, dtype=np.float64)
+    mu = np.mean(variances)
+    return y, std, mu, np.mean(KA[l_last_three_quarters:]), np.mean(KW[l_last_three_quarters:]), np.mean(AW[l_last_three_quarters:]), variances
 
 series_to_plt = {}
 x = ["seed"]
 max_l = 0
 list_of_mu = []
 list_of_std = []
+means_of_KA = []
+means_of_KW = []
+means_of_AW = []
+vars_of_KA = []
+vars_of_KW = []
+vars_of_AW = []
 for i in range(1,int(sys.argv[2])+1):
-    series_to_plt[i], new_mu, new_std  = plot_result(i)
+    series_to_plt[i], new_mu, new_std, sKA, sKW, sAW, vars  = plot_result(i)
     list_of_mu += [new_mu]
     list_of_std += [new_std]
+    means_of_KA += [sKA]
+    means_of_KW += [sKW]
+    means_of_AW += [sAW]
+    vars_of_KA += [vars[0]]
+    vars_of_KW += [vars[1]]
+    vars_of_AW += [vars[2]]
     if len(series_to_plt[i]) > max_l: max_l = len(series_to_plt[i])
 
 
@@ -64,9 +77,18 @@ for i in range(1,max_l):
 
 
 for i in range(len(list_of_mu)):
-    print("Series", i+1, "\t- mean =", str(list_of_mu[i])[:6], "std =", str(list_of_std[i])[:6])
+    print("Series", i+1, "\t- var mean =", str(list_of_mu[i])[:6], "var std =", str(list_of_std[i])[:6], \
+    "KA mean =", str(means_of_KA[i])[:6], "KW mean =", str(means_of_KW[i])[:6], "AW mean =", str(means_of_AW[i])[:6], \
+    "KA var =", str(vars_of_KA[i])[:6], "KW var =", str(vars_of_KW[i])[:6], "AW var =", str(vars_of_AW[i])[:6])
 print("~~~"*15)
-print("Mean of \t- mean =", str(np.mean(list_of_mu))[:6], "std =", str(np.mean(list_of_std))[:6])
+print("Mean of \t- var mean =", str(np.mean(list_of_mu))[:6], "var std =", str(np.mean(list_of_std))[:6], \
+"KA mean =", str(np.mean(means_of_KA))[:6], "KW mean =", str(np.mean(means_of_KW))[:6], "AW mean =", str(np.mean(means_of_AW))[:6], \
+"KA var =", str(np.mean(vars_of_KA))[:6], "KW var =", str(np.mean(vars_of_KW))[:6], "AW var =", str(np.mean(vars_of_AW))[:6])
+print("std of \t\t- var mean =", str(np.std(list_of_mu))[:6], "var std =", str(np.std(list_of_std))[:6], \
+"KA mean =", str(np.std(means_of_KA))[:6], "KW mean =", str(np.std(means_of_KW))[:6], "AW mean =", str(np.std(means_of_AW))[:6], \
+"KA var =", str(np.std(vars_of_KA))[:6], "KW var =", str(np.std(vars_of_KW))[:6], "AW var =", str(np.std(vars_of_AW))[:6])
+
+
 
 fig, ax = plt.subplots()
 for j in range(1,len(series_to_plt)+1):
